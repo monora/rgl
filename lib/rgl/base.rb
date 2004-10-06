f94c3305aa208bb93a9751cec609048d75fa1f3e
Horst Duchene <monora@gmail.com>
2004-10-06 22:11:31 +0000

    Code from utils.rb included

--- lib/rgl/base.rb
+++ lib/rgl/base.rb
@@ -6,7 +6,51 @@
 # library. The main module is RGL::Graph which defines the abstract behavior of
 # all graphs in the library.
 
-RGL_VERSION = "V0.2.1"
+RGL_VERSION = "0.2.1"
+
+unless Enumerable.instance_methods(true).grep(/inject/)
+  module Enumerable
+	def inject(*argv)
+	  argc = argv.size
+
+	  if argc == 0
+		first = true
+		result = nil
+
+		each { |e|
+		  if first
+			first = false
+			result = e
+		  else
+			result = yield(result, e)
+		  end
+		}
+	  elsif argc == 1
+		result = argv[0]
+
+		each { |e| result = yield(result, e) }
+	  else
+		raise ArgumentError, "wrong # of arguments(#{argc} for 1)"
+	  end
+
+	  result
+	end
+  end
+end  
+
+module Enumerable
+  # Fixnum()
+  #
+  # Return the number of elements of the Enumerable. Same as _size_ but not all
+  # Enumerables implement size.
+  #--
+  # Should we call the methods _size_?
+  def length
+	inject(0) do |sum,v|
+	  sum + 1
+	end
+  end
+end
 
 module RGL
   class NotDirectedError < RuntimeError; end
