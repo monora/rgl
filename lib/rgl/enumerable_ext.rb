433c4c3b6e337cbb02d1e999fb6a9f204fbb216e
Horst Duchene <monora@gmail.com>
2008-02-17 20:15:04 +0000

    Removed backwards compatability method inject. Ruby > 1.8 supports it out of the box.

--- lib/rgl/enumerable_ext.rb
+++ lib/rgl/enumerable_ext.rb
@@ -1,33 +1,3 @@
-unless Enumerable.instance_methods(true).grep(/inject/)
-  module Enumerable
-    def inject(*argv)
-      argc = argv.size
-
-      if argc == 0
-        first = true
-        result = nil
-
-        each { |e|
-          if first
-            first = false
-            result = e
-          else
-            result = yield(result, e)
-          end
-        }
-      elsif argc == 1
-        result = argv[0]
-
-        each { |e| result = yield(result, e) }
-      else
-        raise ArgumentError, "wrong # of arguments(#{argc} for 1)"
-      end
-
-      result
-    end
-  end
-end  
-
 module Enumerable
   # Fixnum()
   #
