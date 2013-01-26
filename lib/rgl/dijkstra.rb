3bc814261e18e69cc2957f70c0a89bae3d5fb22c
Kirill Lashuk <kirill.lashuk@gmail.com>
2013-01-26 19:54:04 +0300

    Add Graph#dijkstra_shortest_paths method.

--- lib/rgl/dijkstra.rb
+++ lib/rgl/dijkstra.rb
@@ -48,18 +48,45 @@ module RGL
 
     # Finds the shortest path from the _source_ to the _target_ in the graph.
     #
+    # Returns the shortest path, if it exists, as an Array of vertices. Otherwise, returns nil.
+    #
     def shortest_path(source, target)
+      init(source)
+      relax_edges(target, true)
+      restore_path(source, target)
+    end
+
+    # Finds the shortest path form the _source_ to every other vertex of the graph.
+    #
+    # Returns parents map that can be used to restore the shortest path, if it exists, from the _source_ to any vertext.
+    #
+    def shortest_paths(source)
+      init(source)
+      relax_edges
+      restore_paths(source)
+    end
+
+    private
+
+    def reset
+      @visitor.reset
+      @queue.clear
+    end
+
+    def init(source)
       reset
 
       @visitor.color_map[source]    = :GRAY
       @visitor.distance_map[source] = 0
 
       @queue.push(source, @visitor.distance_map[source])
+    end
 
+    def relax_edges(target = nil, break_on_target = false)
       until @queue.empty?
         u = @queue.pop
 
-        break if u == target
+        break if break_on_target && u == target
 
         @visitor.handle_examine_vertex(u)
 
@@ -74,11 +101,7 @@ module RGL
         @visitor.color_map[u] = :BLACK
         @visitor.handle_finish_vertex(u)
       end
-
-      build_path(target)
     end
-
-    private
 
     def relax_edge(u, v)
       @visitor.handle_examine_edge(u, v)
@@ -104,11 +127,6 @@ module RGL
       end
     end
 
-    def reset
-      @visitor.reset
-      @queue.clear
-    end
-
     def edge_weight(u, v)
       if @graph.directed?
         @edge_weights_map[[u, v]]
@@ -117,18 +135,47 @@ module RGL
       end
     end
 
-    def build_path(target)
-      return unless @visitor.reachable?(target)
+    def restore_path(source, target)
+      PathBuilder.new(source, @visitor.parent_map).path(target)
+    end
 
-      path = [target]
-      last = @visitor.parent_map[target]
+    def restore_paths(source)
+      path_builder = PathBuilder.new(source, @visitor.parent_map)
 
-      while last
-        path.unshift(last)
-        last = @visitor.parent_map[last]
+      @graph.each_vertex do |vertex|
+        path_builder.path(vertex)
       end
 
-      path
+      path_builder.paths
+    end
+
+    class PathBuilder
+
+      attr_reader :paths
+
+      def initialize(source, parent_map)
+        @source     = source
+        @parent_map = parent_map
+        @paths      = {}
+      end
+
+      def path(target)
+        if @paths.has_key?(target)
+          @paths[target]
+        else
+          @paths[target] = restore_path(target)
+        end
+      end
+
+      private
+
+      def restore_path(target)
+        return [@source] if target == @source
+
+        parent = @parent_map[target]
+        path(parent) + [target] if parent
+      end
+
     end
 
     class DijkstraQueue < SimpleDelegator
@@ -160,6 +207,10 @@ module RGL
 
     def dijkstra_shortest_path(edge_weights_map, source, target, visitor = DijkstraVisitor.new(self))
       DijkstraAlgorithm.new(self, edge_weights_map, visitor).shortest_path(source, target)
+    end
+
+    def dijkstra_shortest_paths(edge_weights_map, source, visitor = DijkstraVisitor.new(self))
+      DijkstraAlgorithm.new(self, edge_weights_map, visitor).shortest_paths(source)
     end
 
   end # module Graph
--- test/dijkstra_test.rb
+++ test/dijkstra_test.rb
@@ -23,15 +23,50 @@ class TestDijkstra < Test::Unit::TestCase
     assert_equal([1, 3, 2, 4], shortest_path(1, 4))
   end
 
+  def test_shortest_path_to_the_source
+    assert_equal([1], shortest_path(1, 1))
+  end
+
   def test_path_for_unreachable_vertex
     @graph.add_vertex(5)
     assert_equal(nil, shortest_path(1, 5))
+  end
+
+  def test_shortest_paths_search
+    assert_equal(
+        {
+            1 => [1],
+            2 => [1, 3, 2],
+            3 => [1, 3],
+            4 => [1, 3, 2, 4]
+        },
+        shortest_paths(1)
+    )
+  end
+
+  def test_shortest_paths_search_with_unreachable_vertex
+    @graph.add_vertex(5)
+
+    assert_equal(
+        {
+            1 => [1],
+            2 => [1, 3, 2],
+            3 => [1, 3],
+            4 => [1, 3, 2, 4],
+            5 => nil
+        },
+        shortest_paths(1)
+    )
   end
 
   private
 
   def shortest_path(source, target)
     @graph.dijkstra_shortest_path(@edge_weights, source, target)
+  end
+
+  def shortest_paths(source)
+    @graph.dijkstra_shortest_paths(@edge_weights, source)
   end
 
 end
