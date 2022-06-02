56cf9ecb2cff9a1e2054dd669d9bdb52fb869df9
Horst Duchene <monora@gmail.com>
2022-06-02 14:48:37 +0200

    fix: remove travis

--- lib/rgl/base.rb
+++ lib/rgl/base.rb
@@ -4,7 +4,7 @@
 # library. The main module is RGL::Graph which defines the abstract behavior of
 # all graphs in the library.
 
-RGL_VERSION = "0.5.7"
+RGL_VERSION = "0.5.8"
 
 module RGL
   class NotDirectedError < RuntimeError; end
